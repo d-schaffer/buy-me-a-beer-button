e6fa372a3710f19d89533531e75a58c0be0a3ba0
d-schaffer <dennis.a.schaffer@icloud.com>
2020-08-04 22:41:00 +0200

    fix image default src rename plugin

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import _Vue from "vue";
 
 import DonateButton from "./components/DonateButton.vue";
 
-export function UIPlugin(Vue: typeof _Vue) {
+export function DonatePlugin(Vue: typeof _Vue) {
   Vue.component("DonateButtton", DonateButton);
 }
